4425311c17aadbb48b616a4a37b6efd28d3579fd
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-09-24 21:39:14 -0400

    concert and song functions

--- liri.js
+++ liri.js
@@ -1,25 +1,57 @@
 // Package to set environmental variables
 require("dotenv").config()
 
-var keys = require("./keys.js")
+var axios = require("axios");
 var moment = require("moment")
-var spotify = new Spotify(keys.spotify)
+var Spotify = require("node-spotify-api")
 var fs = require("fs") // for reading files
 
-var cmd = process.arg[2];
+// Importing key data from another file
+var keys = require("./keys.js")
 
+// Reading user input
+var cmd = process.arg[2];
 var term = process.argv.slice(3).join("+");
 
+// Bands in Town API
 function findConcert() {
-
+    var queryURL = `https://rest.bandsintown.com/artists/${term}/events?app_id=codingbootcamp`; 
+    axios.get(queryURL).then(function (response) {
+        for (var i=0; i < response.data.length; i++) {
+            console.log("--------------");
+            console.log("Venue name: " + response.data[i].venue.name);
+            console.log("Location: " + response.data[i].venue.city + ", " + response.data[0].venue.region);
+            console.log("Date: " + moment(response.data[i].datetime).format('MM/DD/YYYY'));
+            console.log("--------------");
+        }
+    }).catch(function (erro) {
+        console.log(error);
+    });
 }
 
+// Spotify API
 function findSong() {
-    
+    var spotify = new Spotify (keys.spotify);
+    spotify.search({type: 'track', query: term })
+    .then(function (response) {
+        var song = response.tracks.items[0];
+        console.log("--------------");
+        console.log("Artist(s): " + song.artists[0].name);
+        console.log("Song's name: " + song.name);
+        console.log("Spotify link: " + song.external_urls.spotify);
+        console.log("Album: " + song.album.name);
+        console.log("--------------");
+    }).catch(function (error) {
+        console.log(error);
+    })
 }
 
+// MDb API call
 function findMovie() {
-    
+    var queryURL = `http://www.omdbapi.com/?apikey=d690e4f5&t=${term}`;
+    axios.get(queryURL).then(function (response) {
+        
+    })
 }
 
 function txtf() {
